0982b42c73c1d3886f1a63b5de1e2036f8fe21d4
fudomori <fudomori@gmail.com>
2022-06-14 20:50:30 +0300

    My Day 1 in School 21 Sber

---
+++ src/quest4.sh
@@ -0,0 +1,4 @@
+chmod u+x ai_door_control.sh
+bash ai_door_management_module.sh
+^C
+pkill -f ai_door_control.sh
---
+++ src/quest6.sh
@@ -0,0 +1,10 @@
+cd ai_help
+bush keygen.sh
+cd key
+mkdir keys
+mv *.key keys
+mv keys ..
+cd ..
+rm -r key
+mv keys key
+bash unifier.sh
